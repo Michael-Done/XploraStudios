5869b8de9ebce6207a6ce453b7a4c59a6f4d0f99
Michael <michael.done@sympatico.ca>
2017-05-06 19:19:07 -0400

    Initial commit

---
+++ android/src/com/project_xplorer/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.project_xplorer.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.project_xplorer.game.ProjectXplorerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ProjectXplorerGame(), config);
+	}
+}
---
+++ desktop/src/com/project_xplorer/game/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.project_xplorer.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.project_xplorer.game.ProjectXplorerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ProjectXplorerGame(), config);
+	}
+	
+	void fixBug1() {}
+}
