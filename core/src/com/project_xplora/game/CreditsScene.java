21e655ab25927f6ba725a1bb8db5fa0e75c20a9d
Cyrus Gandevia <cyrus.gandevia@gmail.com>
2017-05-31 08:51:23 -0400

    Updated Credits

---
+++ core/src/com/project_xplora/game/CreditsScene.java
@@ -0,0 +1,32 @@
+package com.project_xplora.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class CreditsScene extends GameObjectController {
+	private SpriteBatch backgroundBatch;
+	private Texture backgroundTexture;
+
+	public CreditsScene(Settings settings) {
+		super(settings);
+		backgroundBatch = new SpriteBatch();
+		backgroundTexture = new Texture(Gdx.files.internal("CreditsScene.jpg"));
+	}
+
+	public void update() {
+		backgroundBatch.begin();
+		backgroundBatch.draw(backgroundTexture, 0, 0);
+		backgroundBatch.end();
+	}
+	
+
+	@Override
+	public void disposeAll() {
+		backgroundBatch.dispose();
+		backgroundTexture.dispose();
+	}
+
+}
---
+++ core/src/com/project_xplora/game/HighScoresScene.java
@@ -0,0 +1,10 @@
+package com.project_xplora.game;
+
+public class HighScoresScene extends GameObjectController {
+
+	public HighScoresScene(Settings settings) {
+		super(settings);
+		// TODO Auto-generated constructor stub
+	}
+
+}
