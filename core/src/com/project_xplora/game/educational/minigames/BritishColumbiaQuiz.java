e723e2a2fd80637169f82bf9b3cd53f8369cbc55
Cyrus Gandevia <cyrus.gandevia@gmail.com>
2017-06-04 16:22:08 -0400

    Added second minigame + assets

--- core/src/com/project_xplora/game/educational/minigames/BritishColumbiaQuiz.java
+++ core/src/com/project_xplora/game/educational/minigames/BritishColumbiaQuiz.java
@@ -2,15 +2,6 @@ package com.project_xplora.game.educational.minigames;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.project_xplora.game.GameObjectController;
 import com.project_xplora.game.Settings;
 
 public class BritishColumbiaQuiz extends EducationalQuiz {
--- core/src/com/project_xplora/game/educational/minigames/WorldWar2Quiz.java
+++ core/src/com/project_xplora/game/educational/minigames/WorldWar2Quiz.java
@@ -1,5 +1,30 @@
 package com.project_xplora.game.educational.minigames;
 
-public class WorldWar2Quiz {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.project_xplora.game.Settings;
 
+public class WorldWar2Quiz extends EducationalQuiz {
+
+	public WorldWar2Quiz(Settings currentSettings) {
+		super(currentSettings);
+		super.addElements("When did World War II begin?", "1939", "1945", "1960", "a");
+		super.addElements("Who was the leader of Germany during 1939 to 1945?", "Adolf Hitler", "Benito Mussolini",
+				"Joseph Stalin", "a");
+		super.addElements("What event began World War II?", "Pearl Harbor", "Battle of Britain", "Invasion of Poland",
+				"c");
+		super.addElements("When did the bombing of Pearl Harbor happen?", "December 7th, 1941", "November 7th, 1941",
+				"January 27th, 1945", "a");
+		super.addElements("The main Axis powers of World War II consisted of?", "Germany, Russia, Japan",
+				"Germany, Italy, Japan", "Germany, Italy, Romania", "b");
+		super.addElements("Who was the Japanese admiral behind the Pearl Harbor attack?", "Myamoto", "Hirohito",
+				"Yamamoto", "c");
+		super.addElements("What country instigated conflict in North Africa?", "Germany ", "Italy ", "Britain", "a");
+		super.addElements("Which European country did not participate in World War II?", "Spain", "France", "Germany",
+				"a");
+		super.addElements("When did D-Day happen?", "June 6th, 1944", "February 4th, 1944", "June 23rd, 1945", "a");
+		super.addElements("Which country lost the most amount of people in the war?", "Germany", "Russia", "Canada",
+				"b");
+		super.addQuestionTexture(new Texture(Gdx.files.internal("MinigameLevel2Screen.jpg")));
+	}
 }
