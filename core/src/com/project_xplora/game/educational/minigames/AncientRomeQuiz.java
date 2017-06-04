b16f375367040ac9c87f07cdeece4ca269da4806
Cyrus Gandevia <cyrus.gandevia@gmail.com>
2017-06-04 17:10:51 -0400

    Added stuff for minigame

--- core/src/com/project_xplora/game/educational/minigames/AncientRomeQuiz.java
+++ core/src/com/project_xplora/game/educational/minigames/AncientRomeQuiz.java
@@ -1,5 +1,24 @@
 package com.project_xplora.game.educational.minigames;
 
-public class AncientRomeQuiz {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.project_xplora.game.Settings;
+
+public class AncientRomeQuiz extends EducationalQuiz {
+
+	public AncientRomeQuiz(Settings settings) {
+		super(settings);
+		super.addElements("Who was the first Emperor of Rome?", "Julius Caesar", "Marc Antony", "Caius Cassius", "a");
+		super.addElements("What material did Ancient Romans primarily use to build their structures?", "Concrete", "Brick", "Limestone", "c");
+		super.addElements("", "", "", "", "");
+		super.addElements("", "", "", "", "");
+		super.addElements("", "", "", "", "");
+		super.addElements("", "", "", "", "");
+		super.addElements("", "", "", "", "");
+		super.addElements("", "", "", "", "");
+		super.addElements("", "", "", "", "");
+		super.addElements("", "", "", "", "");
+		super.addQuestionTexture(new Texture(Gdx.files.internal("MinigameLevel3Screen.jpg")));
+	}
 
 }
